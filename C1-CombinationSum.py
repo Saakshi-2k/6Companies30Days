1f56093d63aa70366c1976df05f73cf8e4204890
Noob coder <74593820+Saakshi-2k@users.noreply.github.com>
2023-01-03 16:10:19 +0530

    Question2 added

--- C1-CombinationSum.py
+++ C1-CombinationSum.py
@@ -1,1 +1,22 @@
-#Combination sum with a twist 
+# Combination sum with a twist 
+# Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target.
+# LeetCode problem: 39. Combination Sum
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+            if i >= len(candidates) or total > target:
+                return
+
+            cur.append(candidates[i])
+            dfs(i, cur, total+candidates[i])
+            cur.pop()
+            dfs(i+1, cur, total)
+
+        dfs(0, [], 0)
+        return res
